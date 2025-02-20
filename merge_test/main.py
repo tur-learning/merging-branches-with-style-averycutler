c588d85deef9347138340b89217a0b2774f23118
Avery Cutler <tut70526@temple.edu>
2025-02-20 18:09:58 +0000

    calling add function from the main

--- merge_test/main.py
+++ merge_test/main.py
@@ -1,4 +1,12 @@
 from utils import add
+# the above also executes the code of that module, that's why the print function of that script ran
+# so, don't put code in the script you're importing
 
 def main():
-    print("You are on master branch")
+    print("You are on feature a branch")
+    a = 10
+    b = 50
+    result = add(a,b)
+    print(f"The result of the addition is {result}")
+
+main()
